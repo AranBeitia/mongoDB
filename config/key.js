5ed25823fbce2e84709dad8ac3b8595a40bafa42
AranBeitia <arantza.beitia@gmail.com>
2023-05-22 20:02:20 +0200

    Feat: middlewares

--- config/key.js
+++ config/key.js
@@ -1,4 +1,5 @@
 module.exports = {
 	MONGO_URI:
 		'mongodb+srv://arantzafrontend:pQBPOvfngZA3nHVD@cluster0.zhys1fg.mongodb.net/?retryWrites=true&w=majority',
+	jwt_secret: 'mimamamemima',
 }
--- routes/products.js
+++ routes/products.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const ProductController = require('../controllers/ProductController')
+const { authentication, isAdmin } = require('../middlewares/authentication')
 
-router.post('/', ProductController.create)
+router.post('/', authentication, isAdmin, ProductController.create)
 router.get('/getAll', ProductController.getAll)
 router.get('/:_id', ProductController.getById)
 router.get('/name/:name', ProductController.getProductsByName)
-router.delete('/id/:_id', ProductController.delete)
-router.put('/id/:_id', ProductController.update)
+router.delete('/id/:_id', authentication, isAdmin, ProductController.delete)
+router.put('/id/:_id', authentication, isAdmin, ProductController.update)
 
 module.exports = router
